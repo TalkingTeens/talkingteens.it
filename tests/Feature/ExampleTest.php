881402f93050f18646ebeeaed83f265cf444cfeb
Igor <igorliutcanov@gmail.com>
2024-05-20 19:06:32 +0200

    test: github actions

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,10 +9,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-//    public function test_the_application_returns_a_successful_response(): void
-//    {
-//        $this->refreshApplicationWithLocale('it');
-//
-//        $this->get('/it')->assertOk();
-//    }
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $this->refreshApplicationWithLocale('it');
+
+        $this->get('/it')->assertOk();
+    }
 }
